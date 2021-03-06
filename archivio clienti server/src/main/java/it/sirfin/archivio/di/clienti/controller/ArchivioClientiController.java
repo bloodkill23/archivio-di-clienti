ade5496dc4854ba9d01e14411e5dc7eb0306885a
marcobonaccorso <marco.bonaccorso00@gmail.com>
2021-03-06 19:47:09 +0100

    implementazione metodi nel controller

--- archivio clienti server/src/main/java/it/sirfin/archivio/di/clienti/controller/ArchivioClientiController.java
+++ archivio clienti server/src/main/java/it/sirfin/archivio/di/clienti/controller/ArchivioClientiController.java
@@ -5,10 +5,44 @@
  */
 package it.sirfin.archivio.di.clienti.controller;
 
+import it.sirfin.archivio.di.clienti.dto.ClienteDto;
+import it.sirfin.archivio.di.clienti.dto.CriterioRicercaDto;
+import it.sirfin.archivio.di.clienti.dto.ListaClientiDto;
+import it.sirfin.archivio.di.clienti.service.ClientiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Francesco Lillo
  */
+@CrossOrigin("*")
+@RestController
 public class ArchivioClientiController {
-    
+
+    @Autowired
+    ClientiService GestioneClientiService;
+
+    @RequestMapping("/inserisciCliente")
+    @ResponseBody
+    public ListaClientiDto inserisciCliente(@RequestBody ClienteDto dto) {
+        System.out.println("sono in inserisci cliente" + dto);
+        return GestioneClientiService.inserisciCliente(dto.getCliente());
+    }
+
+    @RequestMapping("/ricerca-cliente")
+    @ResponseBody
+    public ListaClientiDto ricercaCliente(@RequestBody CriterioRicercaDto dto) {
+        return GestioneClientiService.ricercaCliente(dto.getCriterio());
+    }
+
+    @RequestMapping("/cancella-cliente")
+    @ResponseBody
+    public ListaClientiDto cancellaCliente(@RequestBody ClienteDto dto) {
+        return GestioneClientiService.cancellaCliente(dto.getCliente());
+    }
 }
