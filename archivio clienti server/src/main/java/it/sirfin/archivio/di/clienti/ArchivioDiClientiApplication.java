c0cb3305fda92f3751417d76ba98571308cd739a
bloodkill23 <78487863+bloodkill23@users.noreply.github.com>
2021-03-06 17:38:02 +0100

    creazione scheletro client e server

---
+++ archivio clienti server/src/main/java/it/sirfin/archivio/di/clienti/ArchivioDiClientiApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivio.di.clienti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioDiClientiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioDiClientiApplication.class, args);
+	}
+
+}
---
+++ archivio clienti server/src/test/java/it/sirfin/archivio/di/clienti/ArchivioDiClientiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivio.di.clienti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivioDiClientiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
